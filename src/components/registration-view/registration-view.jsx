1ebad5a08b543fbb19ca8ce23dbf48f8d862efd4
Sam Garcia <samuel.gsuero@gmail.com>
2022-04-06 09:50:32 +0200

    fixed registration issue - now users can register

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -67,25 +67,25 @@ export function RegistrationView(props) {
               <Form>
                 <Form.Group className="mb-3">
                   <Form.Label>Username:</Form.Label>
-                  <Form.Control type='text' placeholder='Enter a Username' required value={username} onChange={e => setUsername(e.target.value)} />
+                  <Form.Control type='text' placeholder='Enter a Username'  value={username} onChange={e => setUsername(e.target.value)} />
                   {usernameErr && <p>{usernameErr}</p>}
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                   <Form.Label>Password:</Form.Label>
-                  <Form.Control type='password' placeholder='Enter a Password' minLength='8' required value={password} onChange={e => setPassword(e.target.value)} />
+                  <Form.Control type='password' placeholder='Enter a Password' minLength='8'  value={password} onChange={e => setPassword(e.target.value)} />
                   {passwordErr && <p>{passwordErr}</p>}
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                   <Form.Label>Email:</Form.Label>
-                  <Form.Control type='email' placeholder='Enter a Email Address' required value={email} onChange={e => setEmail(e.target.value)} />
+                  <Form.Control type='email' placeholder='Enter a Email Address'  value={email} onChange={e => setEmail(e.target.value)} />
                   {emailErr && <p>{emailErr}</p>}
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                   <Form.Label>Birthday:</Form.Label>
-                  <Form.Control type='date' required value={birthday} onChange={e => setBirthday(e.target.value)} />
+                  <Form.Control type='date'  onChange={e => setBirthday(e.target.value)} />
                 </Form.Group>
 
                 <Button variant='primary' type='submit' onClick={handleSubmit}>Submit</Button>
